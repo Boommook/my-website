e569ecf5a0888e6c99c84554815552fb6136cc0a
Boommook <boommooker@gmail.com>
2025-07-09 13:48:37 -0400

    added soccerball path

--- src/app/soccerball/page.tsx
+++ src/app/soccerball/page.tsx
@@ -2,21 +2,13 @@ import Image from 'next/image'
 
 export default function SoccerballPage() {
   return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      minHeight: '100vh',
-      backgroundColor: 'white'
-    }}>
       <Image
-        src="/soccerball/soccerball.jpg"
+        src="/soccerball.jpg"
         alt="Soccerball"
         width={800}
         height={600}
         style={{ maxWidth: '100%', height: 'auto' }}
         priority
       />
-    </div>
   )
 } 
