a10aaaa7b115d8db6a91074f0ca45a6449a2e9d6
Boommook <boommooker@gmail.com>
2025-08-03 12:40:00 -0400

    cris anniversary present page added

---
+++ src/app/cristina/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { FC } from "react"
+
+const Route: FC = () => {
+    return(
+        <div className="flex flex-col items-center bg-pink-200 h-full">
+            <h1 className="text-center text-4xl font-bold my-10">HAPPY 2 YEAR ANNIVERSARY MY LOVE</h1>
+            <div className="text-left mx-30 text-2xl">
+                Good job solving the rubik's cube!, you've found my secret page mwahahhaha. 
+                I love you so so so so much baby. I'm so glad you're in my life, thank you for the best 2 years of my life.
+                I look forward to spending the rest of my life with you, and I can't wait to see what the future holds for us.
+                You are so perfect mwah mwah mwah.
+                <br/><br/>
+                Sorry this is in my website, I didn't really know where else to put it lol.
+                <br/><br/>
+                Here are some of my favorite memories with you:
+                <br/>
+                <br/>
+                <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                    <img src="/images/cris_anniversary/colecrishozier.jpg" alt="Cristina" className="w-100 rounded-2xl mx-auto"/>
+                    <img src="/images/cris_anniversary/crisdadwedding.jpg" alt="Cristina" className="w-100 rounded-2xl mx-auto"/>
+                    <img src="/images/cris_anniversary/crisandcolepizza.jpg" alt="Cristina" className="w-100 rounded-2xl mx-auto"/>
+                    <img src="/images/cris_anniversary/colepiercingwcris.jpg" alt="Cristina" className="w-100 rounded-2xl mx-auto"/>
+                    
+                </div>
+                <img src="/images/cris_anniversary/coleinaholewcris.jpg" alt="Cristina" className="w-100 rounded-2xl mx-auto my-10"/>
+            </div>
+        </div>
+    );
+}
+
+export default Route;
