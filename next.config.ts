1c9067442ccd011d60bba52cc02145324746a1d4
Boommook <boommooker@gmail.com>
2026-07-06 18:41:25 -0400

    updated nextconfig again

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/~boommook/out",
+  assetPrefix: "/~boommook/out",
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default nextConfig;
