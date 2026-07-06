6d3c50ac39c1ec5dd8f43586717bc7ab8b9c3897
Boommook <boommooker@gmail.com>
2026-07-06 15:37:22 -0400

    Removed window use

--- src/components/projects/ProjectLabels.tsx
+++ src/components/projects/ProjectLabels.tsx
@@ -1,15 +1,17 @@
 import { ReactNode, useEffect, useState } from "react";
 
 export const ProjectLabels = ({ labels }: { labels: ReactNode[] }) => {  
-    const screenWidth = window.innerWidth;
+    const [width, setWidth] = useState<number | null>(null);
+
     const [showLabels, setShowLabels] = useState(5);
     useEffect(() => {
-        if (screenWidth < 1400) {
+        setWidth(window.innerWidth);
+        if (width < 1400) {
             setShowLabels(3);
-        } else if (screenWidth < 1200) {
+        } else if (width < 1200) {
             setShowLabels(3);
         }
-    }, [screenWidth]);
+    }, [width]);
     return (
         <div className="flex justify-center">
             {labels.slice(0, showLabels).map((label, index) => <div key={index}>{label}</div>)}
